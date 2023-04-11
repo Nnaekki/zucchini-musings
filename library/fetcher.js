43a795d1b5a5039a46883d20fbe9d6e96934a919
SANDRA ENUMA <SANDRAENUMA@gmail.com>
2023-04-11 01:31:18 +0100

    fixed fake api

--- library/fetcher.js
+++ library/fetcher.js
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 
-const baseURL = process.env.baseURL;
+const baseURL = "http://localhost:3000/";
 const response = (...args) => fetch(...args).then((res) => res.json());
 
 export default function fetcher(endpoint) {
